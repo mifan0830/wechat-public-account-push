c46ecaba0f2c2eb4d976e9cca0c05c5aabfdca3a
mifan <33972873+mifan0830@users.noreply.github.com>
2023-04-26 15:47:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝0号',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-l-65rkIh4zlWAcbnX5msCZHmls	',
+      id: 'o-l-65rkIh4zlWAcbnX5msCZHmls',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'oLQhvyQwdWPD5ZEleUUDi2hPpctimj6luuWWRnKFUPQ	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-l-65rkIh4zlWAcbnX5msCZHmls	',
+      id: 'o-l-65rkIh4zlWAcbnX5msCZHmls',
     }
   ],
 
